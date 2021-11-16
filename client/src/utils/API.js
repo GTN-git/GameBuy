d308090c547476f7972e17a2e4727fd00b2dae5e
Hok S. Uy <hoksuy@gmail.com>
2021-11-16 11:12:21 -0800

    created API util for querying IGDB API

---
+++ client/src/utils/API.js
@@ -0,0 +1,14 @@
+// Query for searching games{
+export const searchGames = query => {
+    return fetch(`${process.env.REACT_APP_CORS_ANYWHERE}/${process.env.REACT_APP_API_GATEWAY}/v4/games/?search=${query}&fields=id,name,cover.url,category,status`)
+    .then(response => {
+        if(response.ok) {
+            return response.json();
+        }
+    }).then(data => {
+        //console.log(data);
+        return data;
+    }).catch(err => {
+        console.log(err);
+    });
+}
