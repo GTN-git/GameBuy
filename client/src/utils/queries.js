9a57eaf0e1ac4e9c8350f789e8cf12c4b1ac92f7
Cayman Gill <cayman.g@hotmail.com>
2021-11-22 23:22:32 -0500

    updated develop

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,1 +1,17 @@
+import gql from 'graphql-tag';
 
+export const ALL_USERS = gql`
+    {
+        users{
+            username
+            games{
+              name
+              cover
+              rating
+              condition
+              platform
+              price
+            }
+          }
+    }
+`;
